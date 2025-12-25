0a464b0e48ffa8f6feee06982204ac97b8fa4e43
irfansofyana <irfansofyana0305@gmail.com>
2025-12-25 15:41:16 +0700

    fix: set base path to / for custom domain https://starred.irfansp.dev

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: '/my-awesome-stars',
+  base: '/',
   build: {
     format: 'directory',
   },
